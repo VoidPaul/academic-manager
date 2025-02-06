9a8b6c04ea18c469fa46b1abf8ba372c925beba9
Paul Alvarez <palvarez-2023238@kinal.edu.gt>
2025-02-06 06:50:42 -0600

    refactorized: changed code formatting for readability reasons.

--- configs/mongo.js
+++ configs/mongo.js
@@ -3,32 +3,32 @@
 import mongoose from "mongoose"
 
 export const dbConnection = async () => {
-    try {
-        mongoose.connection.on("connecting", () => {
-            console.log("MongoDB | Connecting to MongoDB Service")
-        })
+  try {
+    mongoose.connection.on("connecting", () => {
+      console.log("MongoDB | Connecting to MongoDB Service")
+    })
 
-        mongoose.connection.on("connected", () => {
-            console.log("MongoDB | Connected to MongoDB Service")
-        })
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB | Connected to MongoDB Service")
+    })
 
-        mongoose.connection.on("open", () => {
-            console.log("MongoDB | Connected to MongoDB Database")
-        })
+    mongoose.connection.on("open", () => {
+      console.log("MongoDB | Connected to MongoDB Database")
+    })
 
-        mongoose.connection.on("reconnected", () => {
-            console.log("MongoDB | Reconnected to MongoDB Service")
-        })
+    mongoose.connection.on("reconnected", () => {
+      console.log("MongoDB | Reconnected to MongoDB Service")
+    })
 
-        mongoose.connection.on("disconnected", () => {
-            console.log("MongoDB | Disconnected to MongoDB Service")
-        })
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB | Disconnected to MongoDB Service")
+    })
 
-        await mongoose.connect(process.env.URI_MONGO, {
-            serverSelectionTimeoutMS: 5000,
-            maxPoolSize: 50,
-        })
-    } catch (err) {
-        console.log(`Database connection failed: ${err}`)
-    }
+    await mongoose.connect(process.env.URI_MONGO, {
+      serverSelectionTimeoutMS: 5000,
+      maxPoolSize: 50,
+    })
+  } catch (err) {
+    console.log(`MongoDB | Database connection failed: ${err}`)
+  }
 }
